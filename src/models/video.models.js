b66c8d09c70842100fc85f563f41dbcb0856b1e0
Ketan Gulati <ketangulati66@gmail.com>
2025-04-26 15:45:35 +0530

    user and video data model

---
+++ src/models/video.models.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate";
+
+const videoSchema =  new mongoose.Schema({
+    viddeoFile : {
+        type:String,  // cloudinary url
+        required:true
+    },
+    thumbnail : {
+        type:String,  // cloudinary url
+        required:true
+    },
+    title : {
+        type:String,  
+        required:true
+    },
+    description : {
+        type:String,  
+        required:true
+    },
+    owner : {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    duration : {
+        type:Number,    //cloudinary
+        required:true
+    },
+    views : {
+        type:Number,
+        default:0
+    },
+    isPublished : {
+        type:Boolean,
+        default:true
+    }
+},{timestamps:true})
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video =  mongoose.model("Video",videoSchema)
